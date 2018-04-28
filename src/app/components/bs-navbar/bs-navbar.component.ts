62f9422c7c89e759db6c4e47c726d72d30809ef1
Nikhil Kumar Rustagi <nikhil.rustagi@cybergindia.local>
2018-04-28 17:25:50 +0530

    implemented login logout with angularfireAuth with google login

--- src/app/components/bs-navbar/bs-navbar.component.ts
+++ src/app/components/bs-navbar/bs-navbar.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
-export class BsNavbarComponent implements OnInit {
+export class BsNavbarComponent {
 
-  constructor() { }
+  constructor(private afAuth:AngularFireAuth) { 
+    //For checking what happens when logged in and logging out
+    afAuth.authState.subscribe(res => {
+      console.log(res);
+    })
+    //email, displayName, photoUrl under uid unders /users
 
-  ngOnInit() {
   }
 
+  logout() {
+    this.afAuth
+      .auth
+      .signOut();
+      console.log('logged out')
+  }
 }
