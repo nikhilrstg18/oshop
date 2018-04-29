5adb85763ebef4c884a223fb4642f318b2cf28d6
Nikhil Kumar Rustagi <nikhil.rustagi@cybergindia.local>
2018-04-29 14:15:36 +0530

    implemented search on product

--- src/app/components/admin/admin-products/admin-products.component.ts
+++ src/app/components/admin/admin-products/admin-products.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { ProductService } from '../../../services/product.service';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'admin-products',
@@ -7,13 +8,26 @@ import { ProductService } from '../../../services/product.service';
   styleUrls: ['./admin-products.component.css']
 })
 
-export class AdminProductsComponent {
+export class AdminProductsComponent implements OnDestroy {
 
-  products$
+  products: { key: string, title: string, price: number, category: string, imageUrl: string }[];
+  filteredProducts: any[]
+  subscription: Subscription;
 
   constructor(private productService: ProductService) {
-    this.products$ = productService.getAll();
-   }
+    this.subscription = productService.getAll()
+      .subscribe(p => this.filteredProducts = this.products = p);
+  }
+  
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  filter(query: string) {
+    this.filteredProducts = query
+      ? this.products.filter(p => p.title.toLowerCase().includes(query.toLowerCase()))
+      : this.products;
+  }
 
 
 }
