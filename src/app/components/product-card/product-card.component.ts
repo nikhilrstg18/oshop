2875484edaa3a16330e7ed3fc370efa81e164b08
Nikhil Kumar Rustagi <nikhil.rustagi@cybergindia.local>
2018-04-29 19:46:33 +0530

    add to cart implemented

--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { ShoppingCartService } from './../../services/shopping-cart.service';
+import { Component, Input } from '@angular/core';
 import { Product } from '../../models/product';
 
 @Component({
@@ -6,15 +7,19 @@ import { Product } from '../../models/product';
   templateUrl: './product-card.component.html',
   styleUrls: ['./product-card.component.css']
 })
-export class ProductCardComponent implements OnInit {
+export class ProductCardComponent {
   @Input('product') product:Product;
   @Input('card-width') cardWidth:string;
   @Input('image-height') imageHeight:string;
   @Input('show-actions') showActions:boolean;
 
-  constructor() { }
+  constructor(
+    private cartService:ShoppingCartService
+  ) { }
 
-  ngOnInit() {
+  addToCart(product: Product){
+    this.cartService.addToCart(product);
+    console.log('added to cart');
   }
 
 }
--- src/app/services/shopping-cart.service.ts
+++ src/app/services/shopping-cart.service.ts
@@ -1,8 +1,49 @@
+import { Product } from './../models/product';
+import { AngularFireDatabase } from 'angularfire2/database';
 import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/take';
 
 @Injectable()
 export class ShoppingCartService {
 
-  constructor() { }
+  constructor(
+    private db: AngularFireDatabase
+  ) { }
+
+  async addToCart(product: Product) {
+    let cartId = await this.getOrCreateCartId();
+    let itemRef = this.getItemRef(cartId, product.key);
+    let item$ = itemRef
+      .valueChanges()
+      .take(1)
+      .subscribe(item => {
+        item
+        ? itemRef.update({ product: product, quantity: item['quantity'] + 1 })
+        : itemRef.set({ product: product, quantity: 1 });
+      });
+  }
+
+  private getItemRef(cartId: string, productId: string) {
+    return this.db.object('/shopping-carts/' + cartId + '/items/' + productId);
+  }
+
+  private create() {
+    return this.db.list('shopping-carts/').push({
+      dateCreated: new Date().getTime()
+    });
+  }
+
+  private get(cartId: string) {
+    return this.db.object('/shopping-carts/' + cartId);
+  }
+
+  private async getOrCreateCartId() {
+    let cartId = localStorage.getItem('cartId')
+    if (cartId) return cartId;
+
+    let result = await this.create();
+    localStorage.setItem('cartId', result.key);
+    return result.key;
+  }
 
 }
